44067e803f7ed4c782b7b30727ee60d2ae3ce70a
KitsuneAlex <kitsune-alex@gmx.de>
2018-05-22 18:21:48 +0200

    Bump version

--- src/main/java/com/litewolf101/magicmayhem/util/Constants.java
+++ src/main/java/com/litewolf101/magicmayhem/util/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 
     public static final String MODID = "magicmayhem";
     public static final String NAME = "Wuffy's Magic Mayhem";
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "0.0.3";
     public static final String DEPS = "required-after:codechickenlib;required-after:silverfish;after:jei;after:waila";
     public static final String PROXY_PACKAGE = "com.litewolf101.magicmayhem.proxy";
     public static final String CSIDE = PROXY_PACKAGE + ".ClientProxy";
